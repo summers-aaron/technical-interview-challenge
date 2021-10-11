2b1fade07bb55163a8eec1c44f557ddb1506a483
Aaron Summers <asummers@razoyo.com>
2021-10-11 16:03:21 -0500

    Configured HTTPS

--- config/prod.exs
+++ config/prod.exs
@@ -10,7 +10,7 @@ import Config
 # which you should run after static files are built and
 # before starting your production server.
 config :dogs, DogsWeb.Endpoint,
-  url: [host: System.get_env("HOST_NAME"), port: 443],
+  url: [scheme: "https", host: System.get_env("HOST_NAME"), port: 443],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
